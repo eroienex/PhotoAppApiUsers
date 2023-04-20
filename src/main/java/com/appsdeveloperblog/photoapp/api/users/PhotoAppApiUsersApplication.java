7ff98bba148cbe498308f464fdfce99eccb8f6be
Your Name <you@example.com>
2023-04-20 08:48:22 +0000

    initial ws

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -15,69 +15,8 @@ import feign.Logger;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class PhotoAppApiUsersApplication {
-
-	@Autowired
-	Environment environment;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
 	}
-	
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder()
-	{
-		return new BCryptPasswordEncoder();
-	}
-	
-	@Bean
-	@LoadBalanced
-	public RestTemplate getRestTemplate()
-	{
-		return new RestTemplate();
-	}
-	
-	@Bean
-	@Profile("production")
-	Logger.Level feignLoggerLevel()
-	{
-		return Logger.Level.NONE;
-	}
-	
-	@Bean
-	@Profile("!production")
-	Logger.Level feignDefaultLoggerLevel()
-	{
-		return Logger.Level.FULL;
-	}
-	
-	@Bean
-	@Profile("production")
-	public String createProductionBean() {
-		System.out.println("Production bean created. myapplication.environment = " + environment.getProperty("myapplication.environment"));
-		return "Production bean";
-	}
-	
-	@Bean
-	@Profile("!production")
-	public String createNotProductionBean() {
-		System.out.println("Not Production bean created. myapplication.environment = " + environment.getProperty("myapplication.environment"));
-		return "Not production bean";
-	}
-	
-	@Bean
-	@Profile("default")
-	public String createDevelopmentBean() {
-		System.out.println("Development bean created. myapplication.environment = " + environment.getProperty("myapplication.environment"));
-		return "Development bean";
-	}
-	
-	/*
-	@Bean
-	public FeignErrorDecoder getFeignErrorDecoder()
-	{
-		return new FeignErrorDecoder();
-	} */
-
 }
