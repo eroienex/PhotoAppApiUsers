d3e23027399c373709723925f7d6aef67038f11b
Your Name <you@example.com>
2023-04-24 08:06:17 +0000

    overriding application.properties as parameter on launch and testing load balancer

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -3,16 +3,20 @@ package com.appsdeveloperblog.photoapp.api.users.ui.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
 
+	@Autowired
+	Environment environment;
 	@GetMapping("/status/check")
 	public String status()
 	{
-		return "Working";
+		return "Working on port " + environment.getProperty("local.server.port");
 	}
 
 	
