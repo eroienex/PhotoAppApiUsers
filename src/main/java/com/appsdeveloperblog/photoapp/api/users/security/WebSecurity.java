39c3f8bc85461e24d9824dabba10a18376762c8c
Your Name <you@example.com>
2023-04-28 03:57:28 +0000

    user login

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -1,50 +1,50 @@
 package com.appsdeveloperblog.photoapp.api.users.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
-	
+
 	private Environment environment;
-	
-	@Autowired
-	public WebSecurity(Environment environment) {
+	private UsersService usersService;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	public WebSecurity(Environment environment, UsersService usersService, BCryptPasswordEncoder bCryptPasswordEncoder) {
 		this.environment = environment;
+		this.usersService = usersService;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
-	
+
 	@Bean
 	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		
-		http.csrf().disable();
-		
-		http.authorizeHttpRequests()
-		.requestMatchers(HttpMethod.POST, "/users").access(
-				new WebExpressionAuthorizationManager("hasIpAddress('"+environment.getProperty("gateway.ip")+"')"))
-		.requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-		.and()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		
-		http.headers().frameOptions().disable();
-		
-		return http.build();
-		
-	}
 
+		// Configure AuthenticationManagerBuilder
+		AuthenticationManagerBuilder authenticationManagerBuilder =
+				http.getSharedObject(AuthenticationManagerBuilder.class);
 
+		authenticationManagerBuilder.userDetailsService(usersService)
+				.passwordEncoder(bCryptPasswordEncoder);
 
-	/**@Bean
-	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+		// Create AuthenticationFilter
+		AuthenticationFilter authenticationFilter =
+				new AuthenticationFilter(usersService, environment, authenticationManager);
+		authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
 
 		http.csrf().disable();
 
@@ -52,11 +52,12 @@ public class WebSecurity {
 				.requestMatchers(HttpMethod.POST, "/users").permitAll()
 				.requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
 				.and()
+				.addFilter(authenticationFilter)
+				.authenticationManager(authenticationManager)
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 		http.headers().frameOptions().disable();
 		return http.build();
 
-	}*/
-
+	}
 }
